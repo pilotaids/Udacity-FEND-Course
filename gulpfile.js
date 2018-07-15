8b788875b273eabaaec04a1f2d939510a987574a
pilotaids <marcorosaids@gmail.com>
2018-07-14 20:13:44 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var autoprefixer = require("gulp-autoprefixer");
 var browserSync = require("browser-sync").create();
 var eslint = require("gulp-eslint");
 var jasmine = require("gulp-jasmine-phantom");
+//var jasmineBrowser = require("gulp-jasmine-browser");
 
 
 /*
@@ -16,56 +17,63 @@ var jasmine = require("gulp-jasmine-phantom");
  */
 // Function setting up Sass & Autoprefixer
 gulp.task("styles", function () {
-  gulp.src("sass/**/*.scss")      // Look for sass file in the sass directory and any potential sub-folders
-    .pipe(sass().on("error", sass.logError))  // Convert the file(s) found in css
-    .pipe(autoprefixer({          // Pipe autoprefixer
-            browsers: ["last 2 versions"],
-            cascade: false
-        }))
-    .pipe(gulp.dest("./css"))     // Save the converted file(s) in the css folder
-    .pipe(browserSync.stream());
+	gulp.src("sass/**/*.scss")      // Look for sass file in the sass directory and any potential sub-folders
+		.pipe(sass().on("error", sass.logError))  // Convert the file(s) found in css
+		.pipe(autoprefixer({          // Pipe autoprefixer
+			browsers: ["last 2 versions"],
+			cascade: false
+		}))
+		.pipe(gulp.dest("./css"))     // Save the converted file(s) in the css folder
+		.pipe(browserSync.stream());
 });
 
 
 
 
 gulp.task("lint", function () {
-  return gulp.src(["js/**/*.js"])
-    // eslint() attaches the lint output to the eslint property
-    // of the file object so it can be used by other modules.
-    .pipe(eslint())
-    // eslint.format() outputs the lint results to the console.
-    // Alternatively use eslint.formatEach() (see Docs).
-    .pipe(eslint.format())
-    // To have the process exit with an error code (1) on
-    // lint error, return the stream and pipe to failOnError last.
-    .pipe(eslint.failOnError());
+	return gulp.src(["js/**/*.js"])
+		// eslint() attaches the lint output to the eslint property
+		// of the file object so it can be used by other modules.
+		.pipe(eslint())
+		// eslint.format() outputs the lint results to the console.
+		// Alternatively use eslint.formatEach() (see Docs).
+		.pipe(eslint.format())
+		// To have the process exit with an error code (1) on
+		// lint error, return the stream and pipe to failOnError last.
+		.pipe(eslint.failOnError());
 });
+
+
 
 gulp.task("tests", function () {
-  gulp.src("tests/spec/extraSpec.js")
-    .pipe(jasmine({
-      integration: true,
-      vendor: "js/**/*.js"
-    }));
+	gulp.src("tests/spec/extraSpec.js")
+		.pipe(jasmine({
+			integration: true,
+			vendor: "js/**/*.js"
+		}));
 });
 
+// gulp.task("tests", function() {
+// 	return gulp.src("tests/spec/extraSpec.js")
+// 		.pipe(jasmineBrowser.specRunner({console: true}))
+// 		.pipe(jasmineBrowser.headless({driver: "chrome"}));
+// });
 
 // Collection of the watch functions
 // Ensure 'watch' is also called in the gilp.watch to ensure
 // the watch is re-esatblished upon any change
 gulp.task("watch", function() {
-  // Setting up a 'watch' for 'styles' (css/sass)
-  gulp.watch("sass/**/*.scss", gulp.parallel("styles", "watch"));
-  // Setting up a 'watch' for 'lint' (js)
-  gulp.watch("js/**/*.js", gulp.parallel("lint", "watch"));
+	// Setting up a 'watch' for 'styles' (css/sass)
+	gulp.watch("sass/**/*.scss", gulp.parallel("styles", "watch"));
+	// Setting up a 'watch' for 'lint' (js)
+	gulp.watch("js/**/*.js", gulp.parallel("lint", "watch"));
 });
 
 // Default function called anytime the 'gulp' command is used alone in the
 // command-prompt/terminal without specifying any function(s) after it
 gulp.task("default", gulp.parallel("styles", "lint", "tests", "watch", function() {
-  // Create a browser-sync object and initialize the server
-  browserSync.init({
-    server: "./"
-  });
+	// Create a browser-sync object and initialize the server
+	browserSync.init({
+		server: "./"
+	});
 }));
