0eb1d18e33014c451bd503376046c19c41f4f75b
pilotaids <marcorosaids@gmail.com>
2018-07-14 20:12:56 -0500

    Update extraSpec.js

--- tests/spec/extraSpec.js
+++ tests/spec/extraSpec.js
@@ -1,5 +1,18 @@
-describe('window height', function() {
-	it('returns window height', function() {
+
+describe("window height", function() {
+	it("returns window height", function() {
 		expect(getWindowHeight()).toEqual(jasmine.any(Number));
 	});
 });
+
+// describe("window height", function() {
+//     it("returns window height", function() {
+//         expect(getWindowHeight()).toEqual(jasmine.any(Number));
+//     });
+// });
+
+// describe("addition", function() {
+//  it("return 4 when 2+2", function() {
+//    expect(2+2).toBe(4);
+//  });
+// });
